393e53e695b71a90da49f14480968b0a65c4a311
Virat Patel <21.virat@gmail.com>
2021-04-16 01:08:22 +0530

    dist folder fixed

---
+++ dist/main.d.ts
@@ -0,0 +1,10 @@
+import { RefObject } from 'react';
+
+declare const setBreakpoints: (breaks: [number, number, number, number, number]) => void;
+declare const getBreakpoints: () => [number, number, number, number, number];
+
+declare type screenClass = "xs" | "sm" | "md" | "lg" | "xl" | "xxl";
+declare const screenClasses: screenClass[];
+declare const useScreenClass: <T>(sourceRef?: RefObject<T & HTMLElement> | undefined, defaultClass?: screenClass | undefined) => "xs" | "sm" | "md" | "lg" | "xl" | "xxl";
+
+export { getBreakpoints, screenClasses, setBreakpoints, useScreenClass };
--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,41 @@
 import sass from "rollup-plugin-sass";
 import typescript from "rollup-plugin-typescript2";
-
+import dts from "rollup-plugin-dts";
 import pkg from "./package.json";
 
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: pkg.main,
-      format: "cjs",
-      exports: "named",
-      sourcemap: true,
-      strict: false,
-    },
-  ],
-  plugins: [
-    sass({ insert: true }),
-    typescript({
-      objectHashIgnoreUnknownHack: true,
-    }),
-  ],
-  external: ["react", "react-dom"],
-};
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: pkg.main,
+        format: "cjs",
+        exports: "named",
+        sourcemap: true,
+        strict: false,
+      },
+    ],
+    plugins: [
+      sass({ insert: true }),
+      typescript({
+        clean: true,
+        objectHashIgnoreUnknownHack: true,
+        tsconfigOverride: {
+          compilerOptions: { declaration: true },
+        },
+      }),
+    ],
+    external: ["react", "react-dom"],
+  },
+  {
+    input: "dist/index.d.ts",
+    output: [
+      {
+        file: "dist/main.d.ts",
+        format: "es",
+      },
+    ],
+    plugins: [dts()],
+    external: ["react", "react-dom"],
+  },
+];
